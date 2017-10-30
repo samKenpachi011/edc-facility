91b757582850351bd2d32daa111593e2a8cd8902
erikvw <ew2789@gmail.com>
2017-10-30 13:30:47 +0200

    change app_config to instantiate facility in realtime

--- edc_facility/apps.py
+++ edc_facility/apps.py
@@ -8,7 +8,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.management.color import color_style
 
 from .facility import Facility
-# from .holidays import get_holidays
 
 style = color_style()
 
@@ -17,34 +16,41 @@ class AppConfig(DjangoAppConfig):
     _holidays = {}
     name = 'edc_facility'
     verbose_name = "Edc Facility"
-    holiday_path = settings.HOLIDAY_FILE
-    holiday_model = 'edc_facility.holiday'
-
-    facilities = {
-        'clinic': Facility(
-            name='clinic',
-            days=[MO, TU, WE, TH, FR],
-            slots=[100, 100, 100, 100, 100],
-            country='botswana')}
+    definitions = {
+        'clinic': dict(days=[MO, TU, WE, TH, FR],
+                       slots=[100, 100, 100, 100, 100])}
 
     def ready(self):
-
         sys.stdout.write(f'Loading {self.verbose_name} ...\n')
+        try:
+            holiday_path = settings.HOLIDAY_FILE
+        except AttributeError:
+            holiday_path = None
+        else:
+            if not os.path.exists(holiday_path):
+                sys.stdout.write(style.ERROR(
+                    f'File not found! settings.HOLIDAY_FILE=\'{holiday_path}\'. \n'))
+            else:
+                sys.stdout.write(
+                    f' * reading holidays from {holiday_path}.\n')
         for facility in self.facilities.values():
             sys.stdout.write(f' * {facility}.\n')
-            if self.holiday_path:
-                if not os.path.exists(self.holiday_path):
-                    sys.stdout.write(style.ERROR(
-                        f'File not found! settings.HOLIDAY_FILE=\'{self.holiday_path}\'. \n'))
-                else:
-                    sys.stdout.write(
-                        f' * reading holidays from {self.holiday_path}.\n')
         sys.stdout.write(f' Done loading {self.verbose_name}.\n')
 
+    @property
+    def facilities(self):
+        """Returns a dictionary of facilities.
+        """
+        return {k: Facility(name=k, **v)
+                for k, v in self.definitions.items()}
+
     def get_facility(self, name=None):
+        """Returns a facility instance for this name
+        if it exists.
+        """
         try:
-            facility = self.facilities[name]
+            options = self.definitions[name]
         except KeyError:
             raise ImproperlyConfigured(
-                f'Error creating appointment. Facility {name} does not exist.')
-        return facility
+                f'Facility {name} does not exist. See {self.name} app_config.definitions')
+        return Facility(name=name, **options)
